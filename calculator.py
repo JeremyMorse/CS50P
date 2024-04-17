b74b5a3d9376a267c535a1bc6729c2c520112ef5
Jeremy Morse <jeremyrmorse@gmail.com>
2024-04-17 06:52:20 -0400

    3 ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss

---
+++ calculator.py
@@ -0,0 +1,21 @@
+#Version 2
+
+def main():
+    x = int(input("What's x? "))
+    print("x squared is", square(x))
+
+
+
+#Version 1
+"""
+#Gather Values from User
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+#z = round(x / y, 2)
+z = (x / y)
+
+#Display Output
+#print(f"{z:,}")
+print(f"{z:.2f}")
+"""
--- hello.py
+++ hello.py
@@ -1,5 +1,21 @@
+#Version 2
+def hello(to="world"):
+    print("Hello", to)
+
+def main():
+    name = input ("What's your name? ")
+    hello(f"{name}")
+
+main()
+
+#VERSION 1
 #Ask for name, strip, and title
-name = input ("What's your name? ").strip().title()
+#name = input ("What's your name? ").strip().title()
+
+
+
+#Split users name into first + last
+#name = name.split(" ")
 
 #Say "Hello" to user
 #print ("hello," + " " + name)
@@ -9,7 +25,7 @@ name = input ("What's your name? ").strip().title()
 #print ("hello, ", end="")
 #print(name)
 #-
-print (f"hello, {name}")
+#print (f"hello, {name}")
 
 
 #Cornercase: quotes in quotes
